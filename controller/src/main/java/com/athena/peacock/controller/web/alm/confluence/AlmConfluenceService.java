33d820967a83c8425ca377526cb1d16600336e92
jshan@osci.kr <wjdtn#1212>
2014-09-19 10:20:24 +0900

    Confluence Property change

--- controller/src/main/java/com/athena/peacock/controller/web/alm/confluence/AlmConfluenceService.java
+++ controller/src/main/java/com/athena/peacock/controller/web/alm/confluence/AlmConfluenceService.java
@@ -32,44 +32,58 @@ import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 import org.apache.xmlrpc.client.XmlRpcClientException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.athena.peacock.controller.web.alm.confluence.dto.SpaceSummaryDto;
 import com.athena.peacock.controller.web.alm.crowd.dto.AlmUserDto;
 import com.athena.peacock.controller.web.common.model.ExtjsGridParam;
 import com.athena.peacock.controller.web.common.model.GridJsonResponse;
+import com.atlassian.crowd.integration.rest.service.factory.RestCrowdClientFactory;
 
 /**
  * <pre>
  * 
  * </pre>
  * 
- * @author Bong-Jin Kwon
+ * @author Dave
  * @version 1.0
  */
 @Service
 public class AlmConfluenceService {
 
-	private static final String JIRA_URI = "http://119.81.162.218:8090";
-	private static final String RPC_PATH = "/rpc/xmlrpc";
-	private static final String USER_NAME = "admin";
-	private static final String PASSWORD = "wjdtn#1212";
+	@Value("#{contextProperties['alm.confluence.url']}")
+	private String JIRA_URI;
+
+	@Value("#{contextProperties['alm.confluence.path']}")
+	private String RPC_PATH;
+
+	@Value("#{contextProperties['alm.confluence.id']}")
+	private String USER_NAME;
+
+	@Value("#{contextProperties['alm.confluence.pw']}")
+	private String PASSWORD;
 
 	private XmlRpcClient rpcClient;
 	private XmlRpcClientConfigImpl config;
 
-	public AlmConfluenceService() throws MalformedURLException {
+	public AlmConfluenceService() {
 		// TODO Auto-generated constructor stub
 
+	}
+
+	@PostConstruct
+	private void init() throws MalformedURLException {
 		config = new XmlRpcClientConfigImpl();
 		config.setServerURL(new URL(JIRA_URI + RPC_PATH));
 		rpcClient = new XmlRpcClient();
 		rpcClient.setConfig(config);
-
 	}
 
 	public void insertUser(AlmUserDto user) {
@@ -98,8 +112,7 @@ public class AlmConfluenceService {
 		loginTokenVector.add(getLoginToken());
 		Object[] spaces = (Object[]) rpcClient.execute("confluence2.getSpaces",
 				loginTokenVector);
-		
-		
+
 		System.out.println(spaces.length);
 
 		// Print projects
@@ -132,15 +145,18 @@ public class AlmConfluenceService {
 			Object[] vector = new Object[2];
 			vector[0] = getLoginToken();
 			vector[1] = spaceKey;
-			Object tmp =  rpcClient.execute("confluence2.getSpacePermissionSets", vector);
-			
+			Object tmp = rpcClient.execute(
+					"confluence2.getSpacePermissionSets", vector);
+
 			// Add permission
 			Object[] vector2 = new Object[3];
 			vector2[0] = getLoginToken();
 			vector2[1] = "COMMENT";
 			vector2[2] = spaceKey;
-			//Object tmp2 = rpcClient.execute("confluence2.addAnonymousPermissionToSpace", vector2);
-			
+			// Object tmp2 =
+			// rpcClient.execute("confluence2.addAnonymousPermissionToSpace",
+			// vector2);
+
 			return tmp;
 
 		} catch (Exception e) {
@@ -148,6 +164,6 @@ public class AlmConfluenceService {
 		}
 
 	}
-	
+
 }
 // end of UserService.java
