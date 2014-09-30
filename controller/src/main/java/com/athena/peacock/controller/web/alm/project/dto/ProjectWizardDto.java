8e5c633e395c4c6cfeb8ce3f83e07710acdd6bae
jshan@osci.kr <wjdtn#1212>
2014-09-30 16:06:03 +0900

    Project Wizard 관련 Process변경

--- controller/src/main/java/com/athena/peacock/controller/web/alm/project/dto/ProjectWizardDto.java
+++ controller/src/main/java/com/athena/peacock/controller/web/alm/project/dto/ProjectWizardDto.java
@@ -6,10 +6,11 @@ import com.athena.peacock.controller.web.alm.crowd.dto.AlmUserDto;
 import com.athena.peacock.controller.web.alm.repository.dto.RepositoryDto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(ignoreUnknown=true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ProjectWizardDto {
 
 	private ProjectDto project;
+	private ProjectTemplateDto template;
 	private List<AlmUserDto> users;
 	private List<ProjectMappingDto> jenkins;
 	private List<ProjectMappingDto> confluence;
@@ -21,6 +22,14 @@ public class ProjectWizardDto {
 
 	public void setProject(ProjectDto project) {
 		this.project = project;
+	}
+
+	public ProjectTemplateDto getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(ProjectTemplateDto template) {
+		this.template = template;
 	}
 
 	public List<AlmUserDto> getUsers() {
