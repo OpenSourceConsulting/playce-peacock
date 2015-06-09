c77b78923c65b3ac1ffd937ede0ae92424778528
nices96 <nices96@gmail.com>
2015-06-09 14:49:08 +0900

    create web ui project using sencha

---
+++ design/build/temp/production/MyApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,3 @@
+// @tag full-page
+// @require /Development/project/hiway/workspace/athena-peacock/design/ext/ext-theme-neptune.js
+// @require /Development/project/hiway/workspace/athena-peacock/design/app.js
