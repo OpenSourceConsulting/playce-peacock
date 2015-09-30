29409271f163e662df52c0531d3cfc6659785aa0
jyy <jyy@osci.kr>
2015-09-30 14:23:52 +0900

    Pool Controller

--- controller/src/main/java/com/athena/peacock/controller/web/ceph/pool/PoolController.java
+++ controller/src/main/java/com/athena/peacock/controller/web/ceph/pool/PoolController.java
@@ -24,10 +24,13 @@ package com.athena.peacock.controller.web.ceph.pool;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.athena.peacock.controller.web.ceph.CephBaseController;
+import com.athena.peacock.controller.web.common.model.SimpleJsonResponse;
 
 /**
  * <pre>
@@ -42,5 +45,160 @@ public class PoolController extends CephBaseController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(PoolController.class);
 
+	/**
+	 * <pre>
+	 * 
+	 * </pre>
+	 * @param jsonRes
+	 * @param dto
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/lspools")
+	public @ResponseBody SimpleJsonResponse listPools(SimpleJsonResponse jsonRes) throws Exception {
+		try {
+			Object response = managementSubmit("/osd/lspools", HttpMethod.GET);
+			jsonRes.setSuccess(true);
+			jsonRes.setData(response);
+			jsonRes.setMsg("Pool List가 정상적으로 조회되었습니다.");
+		} catch (Exception e) {
+			jsonRes.setSuccess(false);
+			jsonRes.setMsg("Pool List 조회 중 에러가 발생하였습니다.");
+			
+			LOGGER.error("Unhandled Expeption has occurred. ", e);
+		}
+		
+		return jsonRes;
+	}
+	
+	/**
+	 * <pre>
+	 * 
+	 * </pre>
+	 * @param jsonRes
+	 * @param dto
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/stats/data")
+	public @ResponseBody SimpleJsonResponse getPoolStats(SimpleJsonResponse jsonRes) throws Exception {
+		try {
+			Object response = managementSubmit("/osd/pool/stats?name='data'", HttpMethod.GET);
+			jsonRes.setSuccess(true);
+			jsonRes.setData(response);
+			jsonRes.setMsg("Pool List가 정상적으로 조회되었습니다.");
+		} catch (Exception e) {
+			jsonRes.setSuccess(false);
+			jsonRes.setMsg("Pool List 조회 중 에러가 발생하였습니다.");
+			
+			LOGGER.error("Unhandled Expeption has occurred. ", e);
+		}
+		
+		return jsonRes;
+	}
+
+	/**
+	 * <pre>
+	 * 
+	 * </pre>
+	 * @param jsonRes
+	 * @param dto
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/stats/data/size")
+	public @ResponseBody SimpleJsonResponse getPoolSize(SimpleJsonResponse jsonRes) throws Exception {
+		try {
+			Object response = managementSubmit("/osd/pool/get?pool='data'&var=size", HttpMethod.GET);
+			jsonRes.setSuccess(true);
+			jsonRes.setData(response);
+			jsonRes.setMsg("Pool List가 정상적으로 조회되었습니다.");
+		} catch (Exception e) {
+			jsonRes.setSuccess(false);
+			jsonRes.setMsg("Pool List 조회 중 에러가 발생하였습니다.");
+			
+			LOGGER.error("Unhandled Expeption has occurred. ", e);
+		}
+		
+		return jsonRes;
+	}
+
+	/**
+	 * <pre>
+	 * 
+	 * </pre>
+	 * @param jsonRes
+	 * @param dto
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/stats/data/pg_num")
+	public @ResponseBody SimpleJsonResponse getPoolPgNum(SimpleJsonResponse jsonRes) throws Exception {
+		try {
+			Object response = managementSubmit("/osd/pool/get?pool='data'&var=pg_num", HttpMethod.GET);
+			jsonRes.setSuccess(true);
+			jsonRes.setData(response);
+			jsonRes.setMsg("Pool List가 정상적으로 조회되었습니다.");
+		} catch (Exception e) {
+			jsonRes.setSuccess(false);
+			jsonRes.setMsg("Pool List 조회 중 에러가 발생하였습니다.");
+			
+			LOGGER.error("Unhandled Expeption has occurred. ", e);
+		}
+		
+		return jsonRes;
+	}
+	
+	/**
+	 * <pre>
+	 * 
+	 * </pre>
+	 * @param jsonRes
+	 * @param dto
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/create/test1")
+	public @ResponseBody SimpleJsonResponse createPool(SimpleJsonResponse jsonRes) throws Exception {
+		try {
+			Object response = managementSubmit("/osd/pool/create?pool='test1'&pg_num=64", HttpMethod.GET);
+			jsonRes.setSuccess(true);
+			jsonRes.setData(response);
+			jsonRes.setMsg("Pool List가 정상적으로 조회되었습니다.");
+		} catch (Exception e) {
+			jsonRes.setSuccess(false);
+			jsonRes.setMsg("Pool List 조회 중 에러가 발생하였습니다.");
+			
+			LOGGER.error("Unhandled Expeption has occurred. ", e);
+		}
+		
+		return jsonRes;
+	}	
+	
+	/**
+	 * <pre>
+	 * 
+	 * </pre>
+	 * @param jsonRes
+	 * @param dto
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/set/data/pg_num")
+	public @ResponseBody SimpleJsonResponse setPoolPgNum(SimpleJsonResponse jsonRes) throws Exception {
+		try {
+			Object response = managementSubmit("/osd/pool/set?pool='data'&var=pg_num&val=128", HttpMethod.GET);
+			jsonRes.setSuccess(true);
+			jsonRes.setData(response);
+			jsonRes.setMsg("Pool List가 정상적으로 조회되었습니다.");
+		} catch (Exception e) {
+			jsonRes.setSuccess(false);
+			jsonRes.setMsg("Pool List 조회 중 에러가 발생하였습니다.");
+			
+			LOGGER.error("Unhandled Expeption has occurred. ", e);
+		}
+		
+		return jsonRes;
+	}
 }
 //end of PoolController.java
