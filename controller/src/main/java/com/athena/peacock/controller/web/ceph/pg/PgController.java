5864cb1db056c4bbe9b05726f8b1f47a247f524f
Ji-Woong Choi <ienvyou@gmail.com>
2015-10-02 16:16:00 +0900

    Add placement group control methods  - getStat  - getDumpContents

--- controller/src/main/java/com/athena/peacock/controller/web/ceph/pg/PgController.java
+++ controller/src/main/java/com/athena/peacock/controller/web/ceph/pg/PgController.java
@@ -22,12 +22,19 @@
  */
 package com.athena.peacock.controller.web.ceph.pg;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.athena.peacock.controller.web.ceph.CephBaseController;
+import com.athena.peacock.controller.web.common.model.SimpleJsonResponse;
 
 /**
  * <pre>
@@ -42,5 +49,57 @@ public class PgController extends CephBaseController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(PgController.class);
 
+	/**
+	 * <pre>
+	 * 관리 대상 Ceph에 대한 Placement Group의 현재 상태에 대한 요약 정보를 제공한다. 
+	 * </pre>
+	 * @param jsonRes
+	 * @param dto
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/stat")
+	public @ResponseBody SimpleJsonResponse getStatus(SimpleJsonResponse jsonRes) throws Exception {
+		try {
+			Object response = managementSubmit("/pg/stat", HttpMethod.GET);
+			jsonRes.setSuccess(true);
+			jsonRes.setData(response);
+			jsonRes.setMsg("Placement Group Status가 정상적으로 조회되었습니다.");
+		} catch (Exception e) {
+			jsonRes.setSuccess(false);
+			jsonRes.setMsg("Placement Group Status조회 중 에러가 발생하였습니다.");
+			
+			LOGGER.error("Unhandled Expeption has occurred. ", e);
+		}
+		
+		return jsonRes;
+	}
+	
+	/**
+	 * <pre>
+	 * Placement Group Map에 대한 각 콘텐츠를 조회할 수 있는 기능을 제공한다. 
+	 * Dump 대상 항목으로는 all | summary | sum | delta | pools | osds | pgs | pgs_brief 의 파라미터를 전달하여 각 결과값을 사용한다.
+	 * </pre>
+	 * @param jsonRes
+	 * @param dto
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/dump/{dumpcontents}")
+	public @ResponseBody SimpleJsonResponse getDump(SimpleJsonResponse jsonRes, @PathVariable("dumpcontents") String dumpContents) throws Exception {
+		try {
+			Object response = managementSubmit("/pg/dump?dumpcontents=" + dumpContents, HttpMethod.GET);
+			jsonRes.setSuccess(true);
+			jsonRes.setData(response);
+			jsonRes.setMsg("Placement Group Map이 정상적으로 조회되었습니다.");
+		} catch (Exception e) {
+			jsonRes.setSuccess(false);
+			jsonRes.setMsg("Placement Group Map 조회 중 에러가 발생하였습니다.");
+			
+			LOGGER.error("Unhandled Expeption has occurred. ", e);
+		}
+		
+		return jsonRes;
+	}
 }
 //end of PgController.java
