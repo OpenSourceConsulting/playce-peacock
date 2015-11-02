c11c452c0f46e9de163a6923611473af66428e3d
THLee <THLee@THLee-PC>
2015-11-02 17:28:27 +0900

    Add JUnit test case for Placement Group Controller

---
+++ controller/src/test/java/com/athena/peacock/controller/web/ceph/pg/PgControllerTest.java
@@ -0,0 +1,74 @@
+package com.athena.peacock.controller.web.ceph.pg;
+
+import static org.junit.Assert.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+import com.athena.peacock.controller.web.BaseControllerTest;
+
+public class PgControllerTest extends BaseControllerTest {
+	@Test
+    public void testStatus() throws Exception {
+    	try {
+			mockMvc.perform(get("/ceph/pg/stat").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+					.andDo(print())
+					.andExpect(status().isOk());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception has occurred.");
+		}
+    }
+	
+	@Test
+    public void testDump() throws Exception {
+    	try {
+			mockMvc.perform(get("/ceph/pg/dump/4.7b").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+					.andDo(print())
+					.andExpect(status().isOk());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception has occurred.");
+		}
+    }
+	
+	@Test
+    public void testVersion() throws Exception {
+    	try {
+			mockMvc.perform(get("/ceph/pg/tell/4.7b/version").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+					.andDo(print())
+					.andExpect(status().isOk());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception has occurred.");
+		}
+    }
+	
+	@Test
+    public void testMap() throws Exception {
+    	try {
+			mockMvc.perform(get("/ceph/pg/map/4.7b").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+					.andDo(print())
+					.andExpect(status().isOk());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception has occurred.");
+		}
+    }
+	
+	@Test
+    public void testStartScrub() throws Exception {
+    	try {
+			mockMvc.perform(get("/ceph/pg/scrub/4.7b").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+					.andDo(print())
+					.andExpect(status().isOk());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception has occurred.");
+		}
+    }
+}
