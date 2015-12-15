4feab699955d46d50578060c1cbee21cd05c90f9
Ji-Woong Choi <ienvyou@gmail.com>
2015-12-15 22:54:59 +0900

    Add new service class for handling openstack

---
+++ controller/src/main/java/com/athena/peacock/controller/web/openstack/OpenStackService.java
@@ -0,0 +1,19 @@
+package com.athena.peacock.controller.web.openstack;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * <pre>
+ * OpenStack Service Class
+ * </pre>
+ * @author Ji-Woong Choi
+ * @version 1.0
+ */
+
+@Service("openstackService")
+public class OpenStackService {
+	protected final Logger LOGGER = LoggerFactory.getLogger(OpenStackService.class);
+	
+}
